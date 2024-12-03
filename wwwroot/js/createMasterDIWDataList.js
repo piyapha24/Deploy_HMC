3fa08a952f6673a49310c7eb6ac8196aea1de424
piyapha24 <pm.piyaphat@gmail.com>
2024-12-03 16:22:16 +0700

    Update K

---
+++ wwwroot/js/createMasterDIWDataList.js
@@ -0,0 +1,104 @@
+﻿const connection = new signalR.HubConnectionBuilder().withUrl("/scrapListHub").build();
+var dates = "";
+$(document).ready(function () {
+    // Initialize DataTable
+    var masterDIWData = $('#dataTable').DataTable({
+        destroy: true,
+        colReorder: {
+            realtime: true
+        },
+        ajax: {
+            url: getMasterDIWData,
+            type: "POST",
+            dataType: "json",
+            data: function (e) {
+                e.date = $("#dates").val();
+                e.type = $("#typeall").val();
+            }, // Adjust based on your JSON structure
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.error('AJAX Error:', textStatus, errorThrown);
+                console.error('Response:', jqXHR.responseText);
+            }
+        },
+        columns: [
+            {
+                data: null, // Use null for dynamic index
+                render: function (data, type, row, meta) {
+                    return meta.row + 1; // Row index + 1
+                }, className: 'text-center'
+            },
+            {
+                data: "type", render: function (data) {
+                    console.log('data', data);
+                    if (data == '1')
+                        return `ชื่อกรมโรงาน`;
+                    else if (data == '2')
+                        return `ประเภทของเสีย`;
+                    else if (data == '3')
+                        return `ชื่อของเสีย`;
+                    else if (data == '4')
+                        return `ประเภทรถ`;
+                }, className: 'text-center'
+            },
+            { data: "name", className: 'text-center' },
+            { data: "createdBy", className: 'text-center' },
+            { data: "updatedBy", className: 'text-center' },
+            {
+                data: "updatedAt",
+                render: function (data) {
+                    const date = new Date(data);
+                    const day = String(date.getDate()).padStart(2, '0');
+                    const month = String(date.getMonth() + 1).padStart(2, '0');
+                    const year = date.getFullYear();
+                    return `${day}-${month}-${year}`;
+                }, className: 'text-center'
+            },
+            {
+                data: "id",
+                render: function (data) {
+                    return `
+                                <div class="flex flex-wrap justify-center items-center">
+                                    <a class="btn btn-border-none mr-2" href="/WEB/Environment/EditMasterDIWData/${data}">
+                                        <div class="w-8 h-8 bg-primary/10 dark:bg-primary/20 text-primary/80 flex items-center justify-center rounded-full">
+                                            <i class="fa-solid fa-pencil w-4 h-4"></i>
+                                        </div>
+                                    </a>
+                                    <div class="flex items-center justify-center">
+                                        <i class="fa-solid fa-chevron-right w-4 h-4 mr-1"></i>
+                                    </div>
+                                </div>
+                            `;
+                }, className: 'text-center w-40'
+            }
+        ]
+    });
+
+    $(".dataTables_filter").hide();
+    $('#searchs').keyup(function () {
+        masterDIWData.search($(this).val()).draw();
+    })
+    $('#typeall').on("change", function () {
+        masterDIWData.ajax.reload();
+    })
+
+    setInterval(() => {
+        if (dates != $("#dates").val()) {
+            dates = $("#dates").val()
+            masterDIWData.ajax.reload();
+        }
+    }, 100)
+
+    // Start SignalR connection
+    connection.start().then(() => {
+        console.log("SignalR Connected");
+
+        // Listen for SignalR event to reload DataTable
+        connection.on("CreateMasterDIWDataList", function (newData) {
+            if (!newData) {
+                console.error("Received data is undefined");
+                return;
+            }
+            masterDIWData.ajax.reload(null, false); // Reload the DataTable without resetting pagination
+        });
+    }).catch(err => console.error(err.toString()));
+});
---
+++ wwwroot/js/sellingqtimetoday.js
@@ -0,0 +1,196 @@
+﻿$(document).ready(function () {
+    // Initialize DataTable
+    var qTimeToDayTable = $('#dataTable').DataTable({
+        destroy: true,
+        colReorder: {
+            realtime: true
+        },
+        ajax: {
+            url: getQtimeToday,
+            type: "POST",
+            dataType: "json",
+            data: function (e) {
+                e.datefor = $('#Datefor').val();
+                e.status = $("#statusall").val();
+                e.types = $("#types").val();
+                e.date = $("#dates").val();
+                e.customerId = $("#CustomerId").val();
+            }, // Adjust based on your JSON structure
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.error('AJAX Error:', textStatus, errorThrown);
+                console.error('Response:', jqXHR.responseText);
+            }
+        },
+        columns: [
+            { data: "plant", className: 'text-center' },
+            { data: "docNo", className: 'text-center' },
+            { data: "sellingItem", className: 'text-center' },
+            { data: "locationName", className: 'text-center' },
+            { data: "customerName", className: 'text-center' },
+            { data: "reqName", className: 'text-center' },
+            {
+                data: "deliveryDate", render: function (data, type, row) {
+                    var approvCustomer = row.approvCustomer;
+                    var approvRequester = row.approvRequester;
+                    const date = new Date(data);
+                    const day = String(date.getDate()).padStart(2, '0');
+                    var monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+                    var month = monthNames[date.getMonth()];  // Get month name (e.g., 'Nov')
+                    const year = date.getFullYear();
+
+                    if (approvRequester == true && approvCustomer == true) {
+                        return `
+                        <div class="flex items-center justify-center text-success"> 
+                            <i class="fa-regular fa-square-check mr-2"></i>
+                            ${day} ${month} ${year}
+                        </div>
+                        `;
+                    } else {
+                        return `
+                        <div class="flex items-center justify-center text-danger">
+                            <i class="fa-regular fa-square-check mr-2"></i>
+                            ${day} ${month} ${year}
+                        </div>
+                        `;
+                    }
+                }, className: 'text-center'
+            },
+            { data: "nextStep", className: 'text-center' },
+            {
+                data: "changeMoving", render: function (data, type, row) {
+                    var id = row.id;
+                    var docNo = row.docNo;
+                    var deliveryDate = row.deliveryDate;
+                    var session = row.session;
+                    var approvCustomer = row.approvCustomer;
+                    var approvRequester = row.approvRequester;
+                    if (data == true) {
+                        return `
+                            <div class="flex justify-center items-center">
+                                <button class="QtimeBox-HMCbook" onclick="changeDeliveryDate('${id}', '${docNo}', '${deliveryDate}', '${session}')" data-tw-toggle="modal" data-tw-target="#basic-modal-preview">
+                                    <p class="text-center m-0">
+                                        เปลี่ยนวัน
+                                    </p>
+                                </button>
+                            </div>
+                        `;
+                    } else if (approvRequester == true && approvCustomer == false) {
+                        return `
+                            <div class="flex justify-center items-center">
+                                <a class="flex items-center mr-3 justify-center QtimeBox-HMCbook">
+                                    <p class="text-center m-0">
+                                        HMC จอง
+                                    </p>
+                                </a>
+                            </div>
+                        `;
+                    } else if (approvRequester == true && approvCustomer == true) {
+                        return `
+                            <div class="flex justify-center items-center">
+                                <a class="flex items-center mr-3 justify-center QtimeBox-complete" href="javascript:;">
+                                    <p class="text-center m-0">
+                                        ยืนยัน
+                                    </p>
+                                </a>
+                            </div>
+                        `;
+                    }
+                }, className: 'text-center'
+            },
+            {
+                data: "status", render: function (data, type, row) {
+                    if (data == true) {
+                        return `
+                            <div class="flex justify-center items-center">
+                                <a class="flex items-center mr-3 justify-center QtimeBox-complete" href="javascript:;">
+                                    <p class="text-center m-0">
+                                        ข้อมูลครบ
+                                    </p>
+                                </a>
+                            </div>
+                        `;
+                    } else {
+                        return `
+                            <div class="flex justify-center items-center">
+                                <a class="flex items-center mr-3 justify-center QtimeBox-HMCbook" href="javascript:;">
+                                    <p class="text-center m-0">
+                                        กรอกข้อมูล
+                                    </p>
+                                </a>
+                            </div>
+                        `;
+                    }
+                }, className: 'text-center'
+            }
+        ],
+        createdRow: function (row, data, dataIndex) {
+            $(row).addClass('intro-x'); // Add your custom class
+        }
+    });
+
+    $(".dataTables_filter").hide();
+    $('#searchs').keyup(function () {
+        qTimeToDayTable.search($(this).val()).draw();
+    })
+    $('#statusall').on("change", function () {
+        qTimeToDayTable.ajax.reload();
+    })
+    $('#types').on("change", function () {
+        qTimeToDayTable.ajax.reload();
+    })
+
+    setInterval(() => {
+        if (dates != $("#dates").val()) {
+            dates = $("#dates").val()
+            qTimeToDayTable.ajax.reload();
+        }
+    }, 100)
+});
+
+function changeDeliveryDate(id, docNo, deliveryDate, session) {
+    $('#frmQtimeToDay')[0].reset();
+    var dateObj = new Date(deliveryDate);
+    // Extract the year, month, and day
+    var year = dateObj.getFullYear();
+    var monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    var month = monthNames[dateObj.getMonth()];  // Get month name (e.g., 'Nov')
+    var day = dateObj.getDate().toString().padStart(2, '0');  // Ensure two-digit day
+    // Format the date as 'dd MMM yyyy'
+    var dateNow = `${day} ${month} ${year}`;
+
+    $('.idSelling').val(id);
+    $('.docNo').val(docNo);
+    $('#delivery-date-fields').val(dateNow);
+    $('#session-fields').val(session);
+
+    $('#id').val(id);
+    $('#deliveryDates').val(dateNow);
+    $('#sessions').val(session);
+}
+
+function submitConfirmDeliveryForm() {
+    // Serialize form data
+    var formData = $('#frmQtimeToDay').serializeArray(); // Converts to array of name-value pairs
+    var id = $('#id').val(); // Retrieve the ID value from the form
+    var deliveryDates = $('#deliveryDates').val(); // Retrieve the ID value from the form
+    var sessions = $('#sessions').val(); // Retrieve the ID value from the form
+    var remark = $('#remark').val(); // Retrieve the ID value from the form
+    // Add ID to form data
+    formData.push({ name: 'id', value: id });
+    formData.push({ name: 'deliveryDates', value: deliveryDates });
+    formData.push({ name: 'sessions', value: sessions });
+    formData.push({ name: 'remark', value: remark });
+    // Send AJAX request
+    $.ajax({
+        url: '/WEB/Customer/ConfirmDelivery',
+        type: 'POST',
+        data: $.param(formData), // Converts array back to a URL-encoded string
+        success: function (response) {
+            window.location.href = response.redirectToUrl;
+        },
+        error: function (xhr, status, error) {
+            // Handle error - display error message
+            alert("Error confirming delivery: " + (xhr.responseJSON?.message || error));
+        }
+    });
+}
